8fd5fd7b720e0cb744dc4981a6520cd268671fe3
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-01-10 15:03:56 +0000

    add bodyParser and sequelize, set res headers

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const sequelize = require("./database");
+
+const authRouter = require("./routes/auth");
 
 const app = express();
-app.listen(8080);
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE, PATCH");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
+app.use(authRouter);
+
+sequelize.sync().then(() => {
+    app.listen(8080);
+}).catch((error) => {
+    console.log(error);
+});
