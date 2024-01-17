b21658cc4450ecb3cd0f56005cd07075dd8d3cd7
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-01-17 13:41:57 +0000

    add authorization header to allowed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE, PATCH");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
     if (req.method === "OPTIONS") {
         return res.sendStatus(200);
