b0f4706abb9173926d928ce3bf3a38d5a3bc5cdc
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-04-08 15:44:45 +0100

    add returning totalExpenses data in getMonthlyIncome

--- controllers/stats.js
+++ controllers/stats.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 
 const Sale = require("../models/Sale/Sale");
+const Supply = require("../models/Supply/Supply");
 
 const { findExistingUser } = require("../constants/functions");
 const Item = require("../models/Item/Item");
@@ -24,17 +25,35 @@ exports.getMonthlyIncome = async (req, res, next) => {
             attributes: ["id"]
         }], attributes: ["id", "date", "UserId", "ContactId"]});
 
+        const allSupplies = await Supply.findAll({ where: { UserId: verifiedUserId, date: {
+            [Sequelize.Op.between]: [firstDayOfMonth, lastDayOfMonth]
+        }}, include: [{
+            model: Item,
+            throught: {
+                attributes: ["id", "purchasePrice", "quantity"]
+            },
+            attributes: ["id"]
+        }], attributes: ["id", "date", "ContactId", "UserId"]});
+
         // console.log(allSales);
         const totalSales = allSales.reduce((total, sale) => {
             const totalOneSale = sale.Items.reduce((totalItems, item) => {
                 const totalOneItem = item.SaleItem.price * item.SaleItem.quantity;
                 return totalItems + totalOneItem;
-            }, 0)
+            }, 0);
             return total + totalOneSale;
         }, 0);
 
+        const totalExpenses = allSupplies.reduce((total, supply) => {
+            const totalOneSupply = supply.Items.reduce((totalItems, item) => {
+                const totalOneItem = item.SupplyItem.purchasePrice * item.SupplyItem.quantity;
+                return totalItems + totalOneItem;
+            }, 0);
+            return total + totalOneSupply;
+        }, 0);
+
       
-        res.status(200).json({ data: {totalSales} });
+        res.status(200).json({ data: {totalSales, totalExpenses} });
     } catch (error) {
         next(error);
     }
