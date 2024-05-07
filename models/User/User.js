c7090ac718f8476cb8ba7c5f4e1f464d6d9f35d3
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-05-07 15:30:02 +0100

    switch to test database

--- models/User/User.js
+++ models/User/User.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 
-const sequelize = require("../../database");
+const sequelize = require("../../test-database");
 
 const User = sequelize.define("User", {
     id: {
